5de7bee82c80564f11c2bf4f823afe7cdb8eac7c
Kris Wallperington <Honibannchi@gmail.com>
2016-06-14 13:59:15 -0700

    Filter out example executables in runalltests.py

--- scripts/runalltests.py
+++ scripts/runalltests.py
@@ -1,15 +1,16 @@
 import os
 import stat
 import subprocess
+import glob
 
 executable = stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH
-for filename in os.listdir('.'):
+for filename in glob.glob("test_*"):
 	if os.path.isfile(filename):
 		st = os.stat(filename)
 		mode = st.st_mode
 		if mode & executable:
 			#print(filename,oct(mode))
-			command	= './'+filename
+			command	= filename
 			output	= subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\
 					.stdout.read()
 			print(output)
