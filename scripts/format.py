8b7b12b467ed12b9b53b1df58781de705399adb0
Kris Wallperington <Honibannchi@gmail.com>
2016-06-16 11:33:43 -0700

    Fix format.py to properly catch file extensions.

--- scripts/format.py
+++ scripts/format.py
@@ -1,18 +1,29 @@
 import platform
 import os
+import os.path
 import subprocess
+import argparse
 
-# First, we build a list of files.
-files		= ''
-command		= "find . -type f -name '*.[c\|cpp\|h]' -print"
-if 'windows' in platform.system().lower():
-	command = "dir /b /s *.c *.h *.cpp"
+filexts = [
+	'.c',
+	'.cpp',
+	'.h',
+	'.hpp',
+]
 
-files		= subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\
-			.stdout.read()
+parser = argparse.ArgumentParser()
+parser.add_argument("--dry-run", help="Show what files would be formatted without performing any modifications.", action="store_true")
+args = parser.parse_args()
 
-print files
-for fname in files.split():
-	#print(fname)
-	os.system("uncrustify -c dotfiles/iondb_style.cfg --replace --no-backup " + fname)
-	subprocess.call(["python", "dotfiles/scripts/spacestotabs.py", fname, fname])
+for root, dirs, files in  os.walk("."):
+	for file_name in files:
+		_, ext = os.path.splitext(file_name)
+		if ext not in filexts:
+			continue
+
+		full_path = os.path.join(root, file_name)
+		if args.dry_run:
+			print(full_path)
+		else:
+			os.system("uncrustify -c dotfiles/iondb_style.cfg --replace --no-backup " + full_path)
+			subprocess.call(["python", "dotfiles/scripts/spacestotabs.py", full_path, full_path])
