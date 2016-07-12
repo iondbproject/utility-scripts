e3aea4865ede7d94b132ecc492172c4c1d9fb7d5
Kris Wallperington <Honibannchi@gmail.com>
2016-07-12 16:13:17 -0700

    Add signal to show bad error codes to runalltests

--- scripts/runalltests.py
+++ scripts/runalltests.py
@@ -5,12 +5,15 @@ import glob
 
 executable = stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH
 for filename in glob.glob("test_*"):
-	if os.path.isfile(filename):
-		st = os.stat(filename)
-		mode = st.st_mode
-		if mode & executable:
-			print("Running " + filename)
-			command	= "./" + filename
-			output	= subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)\
-					.stdout.read()
-			print(output)
+    if os.path.isfile(filename):
+        st = os.stat(filename)
+        mode = st.st_mode
+        if mode & executable:
+            print("Running " + filename)
+            command = "./" + filename
+            proc    = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            output, _ = proc.communicate()
+            print(output)
+
+            if proc.returncode != 0:
+                print("** ERROR ** returned error code {}".format(proc.returncode))
